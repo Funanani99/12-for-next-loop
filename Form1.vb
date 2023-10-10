d9d34a237913284ba80be6360c7e1f02d975b4d8
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 08:58:48 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num As Integer
+        For num = 1 To 5
+            num += 1
+            MsgBox(num)
+            If num = 4 Then
+                Exit For
+            End If
+        Next
+        MsgBox("Out of the loop")
+    End Sub
+End Class
